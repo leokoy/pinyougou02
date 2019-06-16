a8d9433b83e149faa0bfd56185ea8ee4e6325072
leokoy <111@163.com>
2019-06-16 17:27:31 +0800

    1112

--- pinyougou-dao/src/main/java/com/pinyougou/mapper/TbOrderItemMapper.java
+++ pinyougou-dao/src/main/java/com/pinyougou/mapper/TbOrderItemMapper.java
@@ -1,7 +1,12 @@
 package com.pinyougou.mapper;
 
 import com.pinyougou.pojo.TbOrderItem;
+import entity.Order;
 import tk.mybatis.mapper.common.Mapper;
 
+import java.util.List;
+
 public interface TbOrderItemMapper extends Mapper<TbOrderItem> {
+    List<Order> findCount();
+
 }
---
+++ pinyougou-shop-web/src/main/webapp/js/controller/orderCountController.js
@@ -0,0 +1,32 @@
+﻿var app = new Vue({
+    el: "#app",
+    data: {
+        pages:15,
+        pageNo:1,
+        list:[],
+        entity:{},
+        ids:[],
+        searchEntity:{title:''}
+    },
+    methods: {
+
+        findOrderCount: function () {
+            console.log(app);
+            axios.get('/orderItem/findOrderCount.shtml').then(function (response) {
+                console.log(response);
+                //注意：this 在axios中就不再是 vue实例了。
+                app.list = response.data;
+
+            }).catch(function (error) {
+
+            })
+        }
+    },
+
+    //钩子函数 初始化了事件和
+    created: function () {
+        this.findOrderCount();
+
+    }
+
+})
